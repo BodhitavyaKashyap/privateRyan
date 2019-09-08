aac29d4a18670a7f0a3670b688efd3acc425fe10
Raushan Kumar Pandey <raushan.kumar478@gmail.com>
2019-09-08 13:59:57 +0530

    created models

--- privateRyan/instacar/app1/admin.py
+++ privateRyan/instacar/app1/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
-
+from app1 import models as app1models
 # Register your models here.
+admin.site.register(app1models.Driver)
+admin.site.register(app1models.User)
+admin.site.register(app1models.Order)
+admin.site.register(app1models.Destination)
--- privateRyan/instacar/app1/models.py
+++ privateRyan/instacar/app1/models.py
@@ -1,3 +1,29 @@
 from django.db import models
 
 # Create your models here.
+class Driver(models.Model):
+    name = models.CharField(max_length = 50)
+    phonenumber = models.IntegerField()
+    chargeperkm = models.IntegerField()
+    profileurl  = models.CharField(max_length = 100)
+
+class User(models.Model):
+    name = models.CharField(max_length = 50)
+    phonenumber = models.IntegerField()
+    emailid = models.CharField(max_length = 254)
+    password = models.CharField(max_length = 50)
+    chargeperkm = models.IntegerField()
+    profileurl  = models.CharField(max_length = 100)
+
+class Order(models.Model):
+    orderticket = models.IntegerField()
+    customerid = models.ForeignKey(User,on_delete=models.CASCADE)
+    destination= models.CharField(max_length = 50)
+    price = models.IntegerField()
+    driver =  models.ForeignKey(Driver,on_delete=models.CASCADE)
+
+class Destination(models.Model):
+    city = models.CharField(max_length = 50)
+    distance =  models.IntegerField()
+
+
